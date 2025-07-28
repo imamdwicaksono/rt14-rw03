b93d7dc339540692573011169944899c06b76725
Imam Dwicaksono <imam.dwicaksono@mmsgroup.co.id>
2025-07-28 21:05:13 +0700

    Refactor BeritaDetailPage to include Head component for improved SEO and metadata handling

--- app/berita/[slug]/page.tsx
+++ app/berita/[slug]/page.tsx
@@ -6,11 +6,13 @@ import { useParams } from 'next/navigation'
 import { supabase } from '@/lib/supabase'
 import HeaderContent from '@/components/header_content'
 import ShareButtons from '@/components/ShareButtons'
+import Head from 'next/head'
 
 type Berita = {
   id: number
   judul: string
   isi: string
+  slug: string
   gambar_url: string
   created_at: string
 }
@@ -39,27 +41,34 @@ export default function BeritaDetailPage() {
   if (!berita) return <div className="text-center">Loading...</div>
 
   return (
+    <>
+      <Head>
+        <title>{berita.judul}</title>
+        <meta property="og:title" content={berita.judul} />
+        <meta property="og:description" content={berita.slug} />
+      <meta property="og:image" content={berita.gambar_url} />
+      <meta property="og:url" content={`https://rt14.keroncongpermai.com/berita/${berita.slug}`} />
+    </Head>
     <div className="max-w-3xl mx-auto">
-      <HeaderContent
-              h1="📢 Detail Berita & Kegiatan Warga"
-              p="Detail Berita-berita dan kegiatan terbaru warga Beringin 14."
-            />  
-      <div className="p-4 bg-white shadow rounded-xl">
-        <h1 className="mb-2 text-2xl font-bold">{berita.judul}</h1>
-        <p className="mb-3 text-sm text-gray-500">
-          {new Date(berita.created_at).toLocaleDateString('id-ID')}
-        </p>
-        <ShareButtons title={berita.judul}  />
-        {berita.gambar_url && (
-          // eslint-disable-next-line @next/next/no-img-element
-          <img
-            src={berita.gambar_url}
-            alt={berita.judul}
-            className="object-cover w-full mb-4 rounded-md"
-          />
-        )}
-        <p className="text-base leading-relaxed whitespace-pre-line" dangerouslySetInnerHTML={{ __html: berita.isi }} />
+        <HeaderContent
+          h1="📢 Detail Berita & Kegiatan Warga"
+          p="Detail Berita-berita dan kegiatan terbaru warga Beringin 14." />
+        <div className="p-4 bg-white shadow rounded-xl">
+          <h1 className="mb-2 text-2xl font-bold">{berita.judul}</h1>
+          <p className="mb-3 text-sm text-gray-500">
+            {new Date(berita.created_at).toLocaleDateString('id-ID')}
+          </p>
+          <ShareButtons title={berita.judul} />
+          {berita.gambar_url && (
+            // eslint-disable-next-line @next/next/no-img-element
+            <img
+              src={berita.gambar_url}
+              alt={berita.judul}
+              className="object-cover w-full mb-4 rounded-md" />
+          )}
+          <p className="text-base leading-relaxed whitespace-pre-line" dangerouslySetInnerHTML={{ __html: berita.isi }} />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
